1106618a8058d242ff7ae2e4b249f471189c3d4b
akatal <akatal@avitakatal-LTP.DDN.UPES.AC.IN>
2020-06-25 10:57:51 +0530

    Commit 2

--- TestJava/src/Test5.java
+++ TestJava/src/Test5.java
@@ -4,6 +4,8 @@ public class Test5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello Java");
+		System.out.print("This is commit 2");
+		
 	}
 
 }
